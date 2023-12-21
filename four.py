b494482fe705ddc15365d7a462abba08d2a1429a
Amuluru pavan <amulurpavan1@gmail.com>
2023-12-21 11:28:29 +0530

    mm

---
+++ four.py
@@ -0,0 +1,10 @@
+def cal_age(year):
+    currentyear=2023
+    return currentyear-year
+
+
+age=cal_age(1847)
+print(age)
+
+age=cal_age(1947)
+print(age)
---
+++ three.py
@@ -0,0 +1,5 @@
+def get_bal():
+    return 100
+
+x=get_bal()
+print(x)
